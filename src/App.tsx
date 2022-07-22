033e08460c778acf57c499f20b447d9921067ca9
Pedro Tortello <ptortello@hotmail.com>
2022-07-22 16:25:22 -0300

    Adds Loader & LoaderContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import style from 'styles/app';
+import LoaderContext, { useLoader } from 'contexts/LoaderContext';
+import Loader from 'components/Loader';
 import AuthNavigator from 'screens/AuthNavigator';
 
 const App = () => {
   return (
-    <SafeAreaView style={style.container}>
-      <AuthNavigator />
-    </SafeAreaView>
+    <LoaderContext.Provider value={useLoader()}>
+      <SafeAreaView style={style.container}>
+        <Loader />
+        <AuthNavigator />
+      </SafeAreaView>
+    </LoaderContext.Provider>
   );
 };
 
--- src/screens/TaskNavigator.tsx
+++ src/screens/TaskNavigator.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import LoaderContext from 'contexts/LoaderContext';
 import menuOptions from 'utils/menuOptions';
 import global from 'styles/global';
 import { labelStyle } from 'styles/menu';
 import { IUserProfile } from 'components/UserProfile';
-import TaskList from './TaskList';
 import Menu from 'components/Menu';
+import TaskList from './TaskList';
 
 const Drawer = createDrawerNavigator();
 
@@ -18,6 +19,8 @@ interface ITaskNavigator {
 const TaskNavigator: React.FC<ITaskNavigator> = (
   { userProfile, setIsSignedIn }
 ) => {
+  const { showLoader } = useContext(LoaderContext);
+
   const screenOptions = {
     headerShown: false,
     drawerLabelStyle: labelStyle,
@@ -25,35 +28,39 @@ const TaskNavigator: React.FC<ITaskNavigator> = (
   }
 
   return (
-    <NavigationContainer independent={true}>
-      <Drawer.Navigator
-        screenOptions={screenOptions}
-        drawerContent={
-          (props) =>
-            <Menu
-              props={props}
-              setIsSignedIn={setIsSignedIn}
-              userProfile={userProfile}
-            />
-        }
-      >
-        {menuOptions.map(
-          ({backgroundColor, daysAhead, image, title}, key) => {return (
-            <Drawer.Screen name={title} key={key}>
-              {({ navigation }) =>
-                <TaskList
-                  backgroundColor={backgroundColor}
-                  daysAhead={daysAhead}
-                  image={image}
-                  navigation={navigation}
-                  title={title}
+    <>
+      {!showLoader &&
+        <NavigationContainer independent={true}>
+          <Drawer.Navigator
+            screenOptions={screenOptions}
+            drawerContent={
+              (props) =>
+                <Menu
+                  props={props}
+                  setIsSignedIn={setIsSignedIn}
+                  userProfile={userProfile}
                 />
-              }
-            </Drawer.Screen>
-          )}
-        )}
-      </Drawer.Navigator>
-    </NavigationContainer>
+            }
+          >
+            {menuOptions.map(
+              ({backgroundColor, daysAhead, image, title}, key) => {return (
+                <Drawer.Screen name={title} key={key}>
+                  {({ navigation }) =>
+                    <TaskList
+                      backgroundColor={backgroundColor}
+                      daysAhead={daysAhead}
+                      image={image}
+                      navigation={navigation}
+                      title={title}
+                    />
+                  }
+                </Drawer.Screen>
+              )}
+            )}
+          </Drawer.Navigator>
+        </NavigationContainer>
+      }
+    </>
   )
 };
 
